596ead7c975309e582bde81fbb9c7d9642a84993
Eric Pacheco <pacheco.eric.anthony@gmail.com>
2026-04-20 05:20:36 -0400

    fix(test): add json_field to expected COLLECTOR_TYPES registry set

--- tests/test_collectors_misc.py
+++ tests/test_collectors_misc.py
@@ -53,7 +53,7 @@ def test_collector_types_registry():
         "manifest_signal_count", "manifest_signal_velocity",
         "autonomous_signal_rate", "manifest_autonomous_signal_rate",
         "producer_health", "producer_recency", "backlog_health_metric", "system_resources",
-        "stale_branches", "learning_retention",
+        "stale_branches", "learning_retention", "json_field",
     }
     assert set(COLLECTOR_TYPES.keys()) == expected
 
